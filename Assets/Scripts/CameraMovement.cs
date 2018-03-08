68d46ece658ee201a155e8687a21b65a21041228
lukasz-zatorski <lukasz91112@gmail.com>
2018-03-08 11:06:44 +0100

    Fix CameraMovement, Fix BuildSettings

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,21 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     //player position values
     public Transform target;
+    public float smoothing  = 5.0f;
+    Vector3 offset;
+    Vector3 targetCamPosition;
     public float xDistance = -8f;
     public float yDistance = 10f;
     public float zDistance = -5f;
+    Vector3 temp;
 
-    //NOTE: Desired camera rotation: (40, 50, 0)
-
-    void LateUpdate()
-    {
-        var temp = target.position;
+    void Start()
+    {             
+        temp = target.position;   
         temp.x += xDistance;
         temp.y += yDistance;
         temp.z += zDistance;
         transform.position = temp;
+        offset = transform.position - target.position;
+        transform.LookAt(target.transform);
+    }
+    //NOTE: Desired camera rotation: (40, 50, 0)
+    
+    void FixedUpdate()
+    {
+        targetCamPosition = target.position + offset;
+        transform.position = Vector3.Lerp (transform.position, targetCamPosition, smoothing*Time.deltaTime);
     }
 }
