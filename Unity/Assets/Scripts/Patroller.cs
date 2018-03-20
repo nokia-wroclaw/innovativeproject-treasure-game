f54f4cdc0805cdf35324bf669f093bc31b96445c
lukasz-zatorski <lukasz91112@gmail.com>
2018-03-20 01:48:13 +0100

    Add NavMesh
    Added NavMeshAgent and basic enemy movement function

---
+++ Unity/Assets/Scripts/Patroller.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patroller : MonoBehaviour 
+{
+	NavMeshAgent agent;
+	bool patrolling;
+	public Transform[] patrolTargets;
+	private int destPoint;
+	bool arrived;
+	private Animator anim;
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (agent.pathPending)
+		{
+			Debug.Log("PathPending");
+			return;
+		}
+		if(patrolling)
+		{
+			if(agent.remainingDistance < agent.stoppingDistance)
+			{
+				if(!arrived)
+				{
+					arrived = true;
+					StartCoroutine("GoToNextPoint");
+				}
+			}
+			else
+			{
+				arrived = false;
+			}
+		}
+		else
+		{
+			StartCoroutine("GoToNextPoint");
+		}
+		anim.SetFloat("blendSpeed", agent.velocity.sqrMagnitude);
+	}
+	
+	IEnumerator GoToNextPoint()
+	{
+		if(patrolTargets.Length == 0)
+		{
+			yield break;
+		}
+		patrolling = true;
+		yield return new WaitForSeconds(2f);
+		arrived = false;
+		agent.destination = patrolTargets[destPoint].position;
+		destPoint = (destPoint + 1) % patrolTargets.Length;
+	}
+}
